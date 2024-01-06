7bcf486ad0fe6a3725e3aaa1c1a1de5d3d123986
Godbless Nyagawa <godblessnyagawa@gmail.com>
2024-01-06 20:53:23 +0300

    Update Scripts: add Bash scripting guide.

---
+++ scripts/variable.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+
+JINA="Linux Trovalds"
+echo "Hello" $JINA
+
+# getting user input using read
+
+echo Jina lako la kwanza ni nani?
+read JINA_LA_KWANZA
+echo Jina lako la mwisho ni nani?
+read JINA_LA_MWISHO
+
+echo Karibu $JINA_LA_KWANZA $JINA_LA_MWISHO
+
+# comments in bash scripting
+
+# hii ni comment
+echo "Working with Comments
+
+# 
